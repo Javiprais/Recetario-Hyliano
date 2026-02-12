13ba40903f250fa458ec2df6046b207cb6ef2d99
Javier Cabello <jcabdia603a@isidrodearceneguiycarmona.es>
2026-02-12 11:04:25 +0100

    Sentencia SQL para añadir 1 usuario.

---
+++ sql/sentencia.sql
@@ -0,0 +1,3 @@
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) 
+VALUES 
+(1, 'Javi', 'javi@hyrule.com', NULL, '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, NOW(), NOW());
